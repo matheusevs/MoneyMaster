e38ccbd50357520a66af43e2b4fd87dd03d4adc1
MatheusEduardo <matheus.eduardo@versatecnologia.com.br>
2023-12-17 15:04:45 -0300

    feat: Criação da migration

---
+++ database/migrations/2023_12_17_174933_create_despesas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDespesasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('despesas', function (Blueprint $table) {
+            $table->id();
+            $table->string('ano', 4)->nullable(false);
+            $table->string('mes', 10)->unique()->nullable(false);
+            $table->string('tipo', 30)->nullable(false);
+            $table->string('descricao', 255)->nullable(false);
+            $table->float('valor', 8, 2)->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('despesas');
+    }
+}
